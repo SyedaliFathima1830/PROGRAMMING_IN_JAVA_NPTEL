8df85bf83e6110255bd0e54b9c760f4c949b560c
SyedaliFathima1830 <101088900+SyedaliFathima1830@users.noreply.github.com>
2023-02-14 08:46:37 -0500

    Add files via upload

---
+++ week1/Exercise1_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Exercise1_1{
+    public static void main (String []args){
+        Scanner input = new Scanner(System.in);
+        double mark_avg;
+        int result;
+        int i;
+        int s;
+        s = input.nextInt();
+        int [] arr = new int[s];
+        for(i=0;i<arr.length;i++)
+        {
+            arr[i]=input.nextInt();
+        }
+int 
+    }
+}
